b48612bc6ab30c683611a6743999ff91f0093e92
LucaLand <luca.landolfi3@studio.unibo.it>
2023-05-13 14:41:09 +0200

    Sprint 0 - QAK: Reorganized projects (Context based Qak)

--- Sprint-0/WasteTruckRequest_QAK/depositrequesthandler_qak/depositrequesthandler.pl
+++
@@ -1,7 +0,0 @@
-%====================================================================================
-% depositrequesthandler description   
-%====================================================================================
-context(ctxsmartdevice, "127.0.0.1",  "TCP", "8074").
-context(ctxwasteservice, "localhost",  "TCP", "8072").
- qactor( smartdevice, ctxsmartdevice, "external").
-  qactor( depositrequesthandler, ctxwasteservice, "it.unibo.depositrequesthandler.Depositrequesthandler").
--- Sprint-0/transportTrolley_qak/transporttrolley.pl
+++
@@ -1,6 +0,0 @@
-%====================================================================================
-% transporttrolley description   
-%====================================================================================
-context(ctxwasteservice, "localhost",  "TCP", "8072").
- qactor( transporttrolley, ctxwasteservice, "it.unibo.transporttrolley.Transporttrolley").
-  qactor( depositrequesthandler, ctxwasteservice, "it.unibo.depositrequesthandler.Depositrequesthandler").
