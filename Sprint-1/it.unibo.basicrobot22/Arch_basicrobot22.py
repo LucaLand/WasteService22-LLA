2eaa83d9440aa3d708eacbc3ac8cf63bfbbb5cea
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-18 16:21:53 +0200

    Sprint 1 - Qak Models: added BasicRobot22 modded proj

---
+++ Sprint-1/it.unibo.basicrobot22/Arch_basicrobot22.py
@@ -0,0 +1,33 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+with Diagram('basicrobot22Arch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+     with Cluster('ctxwasteservice', graph_attr=nodeattr):
+          custompathexecutor=Custom('custompathexecutor(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctxbasicrobot', graph_attr=nodeattr):
+          basicrobot=Custom('basicrobot','./qakicons/symActorSmall.png')
+          envsonarhandler=Custom('envsonarhandler','./qakicons/symActorSmall.png')
+          coapdispatcher=Custom('coapdispatcher','./qakicons/symActorSmall.png')
+          datacleaner=Custom('datacleaner(coded)','./qakicons/codedQActor.png')
+          distancefilter=Custom('distancefilter(coded)','./qakicons/codedQActor.png')
+     sys >> Edge(color='red', style='dashed', xlabel='sonar', fontcolor='red') >> envsonarhandler
+     basicrobot >> Edge(color='blue', style='solid', xlabel='coapUpdate', fontcolor='blue') >> coapdispatcher
+     coapdispatcher >> Edge(color='blue', style='solid', xlabel='coapUpdate', fontcolor='blue') >> custompathexecutor
+diag
