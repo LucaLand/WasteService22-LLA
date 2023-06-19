6c6f79055e52efd59d5ab83a0b8ac0942ecf9d97
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-19 15:42:47 +0200

    Sprint 2 - Qak Models: General Logical Architecture

---
+++ Sprint-2/GeneralArchitecture2/Arch_generalarchitecturesprint2.py
@@ -0,0 +1,58 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+with Diagram('generalarchitecturesprint2Arch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+     with Cluster('ctxraspberry', graph_attr=nodeattr):
+          led=Custom('led','./qakicons/symActorSmall.png')
+          sonar23=Custom('sonar23','./qakicons/symActorSmall.png')
+          sonardatahandler=Custom('sonardatahandler','./qakicons/symActorSmall.png')
+          sonar=Custom('sonar(coded)','./qakicons/codedQActor.png')
+          datacleaner=Custom('datacleaner(coded)','./qakicons/codedQActor.png')
+          distancefilter=Custom('distancefilter(coded)','./qakicons/codedQActor.png')
+     with Cluster('ctxsmartdevice', graph_attr=nodeattr):
+          smartdevice=Custom('smartdevice(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctxbasicrobot', graph_attr=nodeattr):
+          basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctxwasteservice', graph_attr=nodeattr):
+          wasteservice=Custom('wasteservice','./qakicons/symActorSmall.png')
+          transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
+          custompathexecutor=Custom('custompathexecutor','./qakicons/symActorSmall.png')
+          alarmsimulator=Custom('alarmsimulator','./qakicons/symActorSmall.png')
+     wasteservice >> Edge(color='darkgreen', style='dashed', xlabel='loadrejecetd', fontcolor='darkgreen') >> smartdevice
+     wasteservice >> Edge(color='magenta', style='solid', xlabel='pickupReq', fontcolor='magenta') >> transporttrolley
+     transporttrolley >> Edge( xlabel='robotStateEvent', **eventedgeattr, fontcolor='red') >> sys
+     transporttrolley >> Edge(color='magenta', style='solid', xlabel='move', fontcolor='magenta') >> custompathexecutor
+     transporttrolley >> Edge(color='darkgreen', style='dashed', xlabel='pickupOk', fontcolor='darkgreen') >> wasteservice
+     transporttrolley >> Edge(color='blue', style='solid', xlabel='toggleStop', fontcolor='blue') >> custompathexecutor
+     custompathexecutor >> Edge(color='blue', style='solid', xlabel='cmd', fontcolor='blue') >> basicrobot
+     custompathexecutor >> Edge(color='blue', style='solid', xlabel='coapUpdate', fontcolor='blue') >> custompathexecutor
+     custompathexecutor >> Edge( xlabel='posEvent', **eventedgeattr, fontcolor='red') >> sys
+     custompathexecutor >> Edge(color='darkgreen', style='dashed', xlabel='moveDone', fontcolor='darkgreen') >> transporttrolley
+     alarmsimulator >> Edge(color='blue', style='solid', xlabel='alarm', fontcolor='blue') >> transporttrolley
+     alarmsimulator >> Edge(color='blue', style='solid', xlabel='alarmStop', fontcolor='blue') >> transporttrolley
+     sys >> Edge(color='red', style='dashed', xlabel='robotStateEvent', fontcolor='red') >> led
+     sys >> Edge(color='red', style='dashed', xlabel='sonardata', fontcolor='red') >> sonar23
+     sys >> Edge(color='red', style='dashed', xlabel='obstacle', fontcolor='red') >> sonar23
+     sonar23 >> Edge( xlabel='sonardataAppl', **eventedgeattr, fontcolor='red') >> sys
+     sonar23 >> Edge( xlabel='alarm', **eventedgeattr, fontcolor='red') >> sys
+     sonardatahandler >> Edge(color='blue', style='solid', xlabel='alarm', fontcolor='blue') >> transporttrolley
+     sonardatahandler >> Edge(color='blue', style='solid', xlabel='alarmStop', fontcolor='blue') >> transporttrolley
+     sys >> Edge(color='red', style='dashed', xlabel='sonardataAppl', fontcolor='red') >> sonardatahandler
+diag
