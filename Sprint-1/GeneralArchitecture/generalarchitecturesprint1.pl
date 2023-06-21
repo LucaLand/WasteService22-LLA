d3fe9d755f4b7779ba0fe6e5cf21402c5d898db9
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-21 10:47:02 +0200

    Sprint 1 - QakModels: GeneralArchitectureSprint1_revision (img generator files)

--- Sprint-1/GeneralArchitecture/generalarchitecturesprint1.pl
+++ Sprint-1/GeneralArchitecture/generalarchitecturesprint1.pl
@@ -1,11 +1,9 @@
 %====================================================================================
 % generalarchitecturesprint1 description   
 %====================================================================================
-context(ctxsmartdevice, "localhost",  "TCP", "8074").
 context(ctxbasicrobot, "localhost",  "TCP", "8020").
 context(ctxwasteservice, "localhost",  "TCP", "8072").
  qactor( basicrobot, ctxbasicrobot, "external").
   qactor( wasteservice, ctxwasteservice, "it.unibo.wasteservice.Wasteservice").
   qactor( transporttrolley, ctxwasteservice, "it.unibo.transporttrolley.Transporttrolley").
   qactor( custompathexecutor, ctxwasteservice, "it.unibo.custompathexecutor.Custompathexecutor").
-  qactor( smartdevice, ctxsmartdevice, "it.unibo.smartdevice.Smartdevice").
