cb8dd2a9a8b07a5e31239842dad0edb072e8d606
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-22 18:02:50 +0200

    Sprint 2 Revision - General Architecture updated to Revision

--- Sprint-2/GeneralArchitecture2/Arch_generalarchitecturesprint2.py
+++ Sprint-2/GeneralArchitecture2/Arch_generalarchitecturesprint2.py
@@ -22,8 +22,6 @@ with Diagram('generalarchitecturesprint2Arch', show=False, outformat='png', grap
      with Cluster('ctxraspberry', graph_attr=nodeattr):
           led=Custom('led','./qakicons/symActorSmall.png')
           sonar23=Custom('sonar23','./qakicons/symActorSmall.png')
-          sonar=Custom('sonar(coded)','./qakicons/codedQActor.png')
-          datacleaner=Custom('datacleaner(coded)','./qakicons/codedQActor.png')
      with Cluster('ctxbasicrobot', graph_attr=nodeattr):
           basicrobot=Custom('basicrobot(ext)','./qakicons/externalQActor.png')
      with Cluster('ctxwasteservice', graph_attr=nodeattr):
@@ -31,18 +29,19 @@ with Diagram('generalarchitecturesprint2Arch', show=False, outformat='png', grap
           transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
           custompathexecutor=Custom('custompathexecutor','./qakicons/symActorSmall.png')
           sonardatahandler=Custom('sonardatahandler','./qakicons/symActorSmall.png')
+          ledstateupdater=Custom('ledstateupdater','./qakicons/symActorSmall.png')
      wasteservice >> Edge(color='magenta', style='solid', xlabel='pickupReq', fontcolor='magenta') >> transporttrolley
-     transporttrolley >> Edge( xlabel='robotStateEvent', **eventedgeattr, fontcolor='red') >> sys
      transporttrolley >> Edge(color='magenta', style='solid', xlabel='move', fontcolor='magenta') >> custompathexecutor
      transporttrolley >> Edge(color='darkgreen', style='dashed', xlabel='pickupOk', fontcolor='darkgreen') >> wasteservice
      transporttrolley >> Edge(color='blue', style='solid', xlabel='toggleStop', fontcolor='blue') >> custompathexecutor
      basicrobot >> Edge(color='blue', style='solid', xlabel='coapUpdate', fontcolor='blue') >> custompathexecutor
      custompathexecutor >> Edge(color='blue', style='solid', xlabel='cmd', fontcolor='blue') >> basicrobot
+     custompathexecutor >> Edge(color='blue', style='solid', xlabel='coapUpdate', fontcolor='blue') >> custompathexecutor
      custompathexecutor >> Edge(color='darkgreen', style='dashed', xlabel='moveDone', fontcolor='darkgreen') >> transporttrolley
+     sonar23 >> Edge(color='blue', style='solid', xlabel='coapUpdate', fontcolor='blue') >> sonardatahandler
      sonardatahandler >> Edge(color='blue', style='solid', xlabel='alarm', fontcolor='blue') >> transporttrolley
      sonardatahandler >> Edge(color='blue', style='solid', xlabel='alarmStop', fontcolor='blue') >> transporttrolley
-     sys >> Edge(color='red', style='dashed', xlabel='sonardataAppl', fontcolor='red') >> sonardatahandler
-     sys >> Edge(color='red', style='dashed', xlabel='robotStateEvent', fontcolor='red') >> led
+     transporttrolley >> Edge(color='blue', style='solid', xlabel='coapUpdate', fontcolor='blue') >> ledstateupdater
+     ledstateupdater >> Edge(color='blue', style='solid', xlabel='ledStateUpdate', fontcolor='blue') >> led
      sys >> Edge(color='red', style='dashed', xlabel='sonardata', fontcolor='red') >> sonar23
-     sonar23 >> Edge( xlabel='sonardataAppl', **eventedgeattr, fontcolor='red') >> sys
 diag
