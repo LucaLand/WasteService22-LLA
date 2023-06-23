2ffb9d7be4c04d07e32fae5dc8de6083b205fc53
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-23 15:22:15 +0200

    Sprint 2 - Qak Led fix + sonar simulator disabled

--- Sprint-2/CtxRaspberry/raspberry.pl
+++ Sprint-2/CtxRaspberry/raspberry.pl
@@ -3,7 +3,6 @@
 %====================================================================================
 context(ctxwasteservice, "127.0.0.1",  "TCP", "8072").
 context(ctxraspberry, "localhost",  "TCP", "8076").
- qactor( sonar, ctxraspberry, "sonarSimulator").
-  qactor( datacleaner, ctxraspberry, "rx.dataCleaner").
+ qactor( datacleaner, ctxraspberry, "rx.dataCleaner").
   qactor( led, ctxraspberry, "it.unibo.led.Led").
   qactor( sonar23, ctxraspberry, "it.unibo.sonar23.Sonar23").
