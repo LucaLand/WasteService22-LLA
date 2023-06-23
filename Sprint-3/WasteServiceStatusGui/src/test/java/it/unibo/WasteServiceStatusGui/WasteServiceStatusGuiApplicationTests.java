433063257c6f0ed008f723fce02c5a191bb0797f
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-23 23:20:52 +0200

    Sprint 3 - Test-Unit of Web Gui

--- Sprint-3/WasteServiceStatusGui/src/test/java/it/unibo/WasteServiceStatusGui/WasteServiceStatusGuiApplicationTests.java
+++ Sprint-3/WasteServiceStatusGui/src/test/java/it/unibo/WasteServiceStatusGui/WasteServiceStatusGuiApplicationTests.java
@@ -1,13 +1,55 @@
 package it.unibo.WasteServiceStatusGui;
 
+import it.unibo.coapobs.CoapObserverJava;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class WasteServiceStatusGuiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WSGuiController controller;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+
+
+	/** Before starting this test run the CtxWasteService in order to make the Observer in the Controller fetch the standard data from the Actors
+	 *	Also run CtxRaspberry (sprint3) and it.unibo.basicrobot (Sprint-1)
+	 **/
+	@Test
+	public void test2() throws Exception {
+		CoapObserverJava testObs = new CoapObserverJava("localhost:8072", "ctxwasteservice", "guiupdater");
+		while(testObs.getCurrentState() == null){
+			System.out.println("Waiting CtxWasteService....");
+			Thread.sleep(1000);
+		}
+
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("athome")));
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("LedOff")));
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(model().attribute("pb", "0"));
 	}
 
 }
