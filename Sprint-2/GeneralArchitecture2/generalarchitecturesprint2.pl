00348a2972e70a994394951e43efeaa4f65a4d49
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-24 12:51:43 +0200

    Sprint 2 - GeneralArchitecture Sprint-2 build

--- Sprint-2/GeneralArchitecture2/generalarchitecturesprint2.pl
+++ Sprint-2/GeneralArchitecture2/generalarchitecturesprint2.pl
@@ -1,14 +1,16 @@
 %====================================================================================
 % generalarchitecturesprint2 description   
 %====================================================================================
-context(ctxraspberry, "localhost",  "TCP", "8076").
+context(ctxraspberry, "localhots",  "TCP", "8076").
+context(ctxsmartdevice, "localhots",  "TCP", "8074").
 context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
 context(ctxwasteservice, "localhost",  "TCP", "8072").
- qactor( basicrobot, ctxbasicrobot, "external").
+ qactor( sonar, ctxraspberry, "sonarSimulator").
+  qactor( smartdevice, ctxsmartdevice, "external").
+  qactor( basicrobot, ctxbasicrobot, "external").
   qactor( wasteservice, ctxwasteservice, "it.unibo.wasteservice.Wasteservice").
   qactor( transporttrolley, ctxwasteservice, "it.unibo.transporttrolley.Transporttrolley").
   qactor( custompathexecutor, ctxwasteservice, "it.unibo.custompathexecutor.Custompathexecutor").
-  qactor( sonardatahandler, ctxwasteservice, "it.unibo.sonardatahandler.Sonardatahandler").
-  qactor( ledstateupdater, ctxwasteservice, "it.unibo.ledstateupdater.Ledstateupdater").
   qactor( led, ctxraspberry, "it.unibo.led.Led").
   qactor( sonar23, ctxraspberry, "it.unibo.sonar23.Sonar23").
+  qactor( sonardatahandler, ctxraspberry, "it.unibo.sonardatahandler.Sonardatahandler").
