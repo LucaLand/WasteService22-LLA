34c8993443013fc0db8a086ee0248275ec7f486f
LucaLand <luca.landolfi3@studio.unibo.it>
2023-06-25 19:39:32 +0200

    Rasp Implementation - Sonar fix

--- Implementation/CtxRaspberry/raspberry.pl
+++ Implementation/CtxRaspberry/raspberry.pl
@@ -4,6 +4,5 @@
 context(ctxraspberry, "localhost",  "TCP", "8076").
  qactor( sonar, ctxraspberry, "sonarHCSR04Support23").
   qactor( datacleaner, ctxraspberry, "rx.dataCleaner").
-  qactor( distancefilter, ctxraspberry, "rx.distanceFilter").
   qactor( led, ctxraspberry, "it.unibo.led.Led").
   qactor( sonar23, ctxraspberry, "it.unibo.sonar23.Sonar23").
